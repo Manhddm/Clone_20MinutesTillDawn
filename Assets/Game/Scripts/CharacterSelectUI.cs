7391416bdae7da10aa385192702f68fbe83ce61a
Manhddm <manhducdongyb@gmail.com>
2025-12-26 21:50:40 +0700

    add infor

--- Assets/Game/Scripts/CharacterSelectUI.cs
+++ Assets/Game/Scripts/CharacterSelectUI.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CharacterSelectUI : MonoBehaviour
 {
     public Image characterImageUI;
+    [SerializeField] private TMP_Text characterName;
+    [SerializeField] private TMP_Text characterHP;
+    [SerializeField] private TMP_Text characterDescription;
+    
     private CharacterSelectButton characterSelectButton;
     public void SetCharacterImage(CharacterSelectButton characterSelectButton)
     {
@@ -17,6 +23,10 @@ public class CharacterSelectUI : MonoBehaviour
         this.characterSelectButton.Select();
         
         characterImageUI.sprite = this.characterSelectButton.characterData.uiCharacterSprite;
+        characterName.SetText(characterSelectButton.characterData.name);
+        characterHP.text = characterSelectButton.characterData.maxHealth.ToString();
+       // Debug.Log(characterSelectButton.characterData.maxHealth);
+        //characterDescription.SetText(characterSelectButton.characterData.description); 
     }
 
     public CharacterSelectButton GetCharacterSelectButton()
