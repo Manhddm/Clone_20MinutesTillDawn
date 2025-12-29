64a998b5d9d72f6b404248f6a9477fb6fe3dec04
Manhddm <manhducdongyb@gmail.com>
2025-12-30 00:15:41 +0700

    delete all, lam lam cuoc choi

--- Assets/Game/Scripts/GameManager.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using CommonScripts;
-using UnityEngine;
-
-public class GameManager : Singleton<GameManager>
-{
-    public CharacterData CharacterSelected { get; set; }
-}
--- Assets/Game/Scripts/SceneLoad.cs
+++
@@ -1,8 +0,0 @@
-
-public struct SceneLoad 
-{
-    public const string bootstrap = "Bootstrap";
-    public const string MainMenu = "MainMenu";
-    public const string CharacterSelection = "SelectScene";
-    public const string GamePlay = "GamePlay";
-}
